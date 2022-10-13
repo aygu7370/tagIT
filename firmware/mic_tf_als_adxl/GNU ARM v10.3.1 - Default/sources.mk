9040b7de1703904f4f8a84d9915512b9de2b0872
ayushgupta98 <98ayushgupta@gmail.com>
2022-10-13 16:40:38 -0600

    feat: sensors interfaced for demo

---
+++ firmware/mic_tf_als_adxl/GNU ARM v10.3.1 - Default/sources.mk
@@ -0,0 +1,88 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+S79_SRCS := 
+ASM_SRCS := 
+CPP_SRCS := 
+S79_UPPER_SRCS := 
+S_UPPER_SRCS := 
+O_SRCS := 
+C_UPPER_SRCS := 
+CXX_SRCS := 
+C++_SRCS := 
+OBJ_SRCS := 
+S_SRCS := 
+CC_SRCS := 
+C_SRCS := 
+CC_DEPS := 
+C++_DEPS := 
+EXECUTABLES := 
+OBJS := 
+C_UPPER_DEPS := 
+CXX_DEPS := 
+C_DEPS := 
+CPP_DEPS := 
+
+# Every subdirectory with source files must be described here
+SUBDIRS := \
+. \
+autogen \
+gecko_sdk_4.1.1/hardware/board/src \
+gecko_sdk_4.1.1/hardware/driver/mic/src \
+gecko_sdk_4.1.1/hardware/driver/mx25_flash_shutdown/src/sl_mx25_flash_shutdown_eusart \
+gecko_sdk_4.1.1/platform/CMSIS/RTOS2/Source \
+gecko_sdk_4.1.1/platform/Device/SiliconLabs/EFR32MG24/Source \
+gecko_sdk_4.1.1/platform/common/src \
+gecko_sdk_4.1.1/platform/common/toolchain/src \
+gecko_sdk_4.1.1/platform/driver/i2cspm/src \
+gecko_sdk_4.1.1/platform/driver/leddrv/src \
+gecko_sdk_4.1.1/platform/driver/mvp/src \
+gecko_sdk_4.1.1/platform/emdrv/dmadrv/src \
+gecko_sdk_4.1.1/platform/emlib/src \
+gecko_sdk_4.1.1/platform/micrium_os/common/source/kal \
+gecko_sdk_4.1.1/platform/micrium_os/common/source/lib \
+gecko_sdk_4.1.1/platform/micrium_os/common/source/logging \
+gecko_sdk_4.1.1/platform/micrium_os/common/source/ring_buf \
+gecko_sdk_4.1.1/platform/micrium_os/common/source/rtos \
+gecko_sdk_4.1.1/platform/micrium_os/cpu/source \
+gecko_sdk_4.1.1/platform/micrium_os/kernel/source \
+gecko_sdk_4.1.1/platform/micrium_os/ports/source/generic \
+gecko_sdk_4.1.1/platform/micrium_os/ports/source/gnu \
+gecko_sdk_4.1.1/platform/peripheral/src \
+gecko_sdk_4.1.1/platform/service/device_init/src \
+gecko_sdk_4.1.1/platform/service/hfxo_manager/src \
+gecko_sdk_4.1.1/platform/service/iostream/src \
+gecko_sdk_4.1.1/platform/service/power_manager/src \
+gecko_sdk_4.1.1/platform/service/sleeptimer/src \
+gecko_sdk_4.1.1/platform/service/system/src \
+gecko_sdk_4.1.1/platform/service/udelay/src \
+gecko_sdk_4.1.1/util/third_party/printf \
+gecko_sdk_4.1.1/util/third_party/printf/src \
+gecko_sdk_4.1.1/util/third_party/tensorflow_extra/cmsis/CMSIS/NN/Source/ActivationFunctions \
+gecko_sdk_4.1.1/util/third_party/tensorflow_extra/cmsis/CMSIS/NN/Source/BasicMathFunctions \
+gecko_sdk_4.1.1/util/third_party/tensorflow_extra/cmsis/CMSIS/NN/Source/ConcatenationFunctions \
+gecko_sdk_4.1.1/util/third_party/tensorflow_extra/cmsis/CMSIS/NN/Source/ConvolutionFunctions \
+gecko_sdk_4.1.1/util/third_party/tensorflow_extra/cmsis/CMSIS/NN/Source/FullyConnectedFunctions \
+gecko_sdk_4.1.1/util/third_party/tensorflow_extra/cmsis/CMSIS/NN/Source/NNSupportFunctions \
+gecko_sdk_4.1.1/util/third_party/tensorflow_extra/cmsis/CMSIS/NN/Source/PoolingFunctions \
+gecko_sdk_4.1.1/util/third_party/tensorflow_extra/cmsis/CMSIS/NN/Source/ReshapeFunctions \
+gecko_sdk_4.1.1/util/third_party/tensorflow_extra/cmsis/CMSIS/NN/Source/SVDFunctions \
+gecko_sdk_4.1.1/util/third_party/tensorflow_extra/cmsis/CMSIS/NN/Source/SoftmaxFunctions \
+gecko_sdk_4.1.1/util/third_party/tensorflow_extra/microfrontend/lib \
+gecko_sdk_4.1.1/util/third_party/tensorflow_extra/microfrontend \
+gecko_sdk_4.1.1/util/third_party/tensorflow_extra/siliconlabs \
+gecko_sdk_4.1.1/util/third_party/tensorflow_extra/src \
+gecko_sdk_4.1.1/util/third_party/tflite-micro/tensorflow/lite/c \
+gecko_sdk_4.1.1/util/third_party/tflite-micro/tensorflow/lite/core/api \
+gecko_sdk_4.1.1/util/third_party/tflite-micro/tensorflow/lite/kernels/internal \
+gecko_sdk_4.1.1/util/third_party/tflite-micro/tensorflow/lite/kernels/internal/reference \
+gecko_sdk_4.1.1/util/third_party/tflite-micro/tensorflow/lite/kernels \
+gecko_sdk_4.1.1/util/third_party/tflite-micro/tensorflow/lite/micro \
+gecko_sdk_4.1.1/util/third_party/tflite-micro/tensorflow/lite/micro/arena_allocator \
+gecko_sdk_4.1.1/util/third_party/tflite-micro/tensorflow/lite/micro/cortex_m_generic \
+gecko_sdk_4.1.1/util/third_party/tflite-micro/tensorflow/lite/micro/kernels \
+gecko_sdk_4.1.1/util/third_party/tflite-micro/tensorflow/lite/micro/kernels/cmsis_nn \
+gecko_sdk_4.1.1/util/third_party/tflite-micro/tensorflow/lite/micro/memory_planner \
+gecko_sdk_4.1.1/util/third_party/tflite-micro/tensorflow/lite/schema \
+
