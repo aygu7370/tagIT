5aac396f374264b45a633492e570f42104702ca5
sankalp123321 <sankalp13392>
2022-12-05 23:43:59 -0700

    feat: bsp created

---
+++ firmware/FinalBoardFW/MainBoard/GNU ARM v10.3.1 - Default/objects.mk
@@ -0,0 +1,8 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+USER_OBJS := D:/CUBoulder/LPDET/Project/tagIT/firmware/FinalBoardFW/MainBoard/ml/sensoryhf.a C:/Users/Sankalp/SimplicityStudio/SDKs/gecko_sdk//protocol/bluetooth/lib/EFR32MG24/GCC/libbluetooth.a C:/Users/Sankalp/SimplicityStudio/SDKs/gecko_sdk//protocol/bluetooth/lib/EFR32MG24/GCC/libbtmesh_crypto_key_cache.a C:/Users/Sankalp/SimplicityStudio/SDKs/gecko_sdk//protocol/bluetooth/lib/EFR32MG24/GCC/libbluetooth_mesh.a C:/Users/Sankalp/SimplicityStudio/SDKs/gecko_sdk//platform/emdrv/nvm3/lib/libnvm3_CM33_gcc.a C:/Users/Sankalp/SimplicityStudio/SDKs/gecko_sdk//platform/radio/rail_lib/autogen/librail_release/librail_efr32xg24_gcc_release.a
+
+LIBS := -lgcc -lc -lm -lnosys
+
